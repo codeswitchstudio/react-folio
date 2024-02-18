4d148ea69b2fc49456a393f39092f53600dd4e2c
codeswitchstudio <toolbarista@gmail.com>
2024-02-18 22:56:35 +0000

    provisional projects gallery

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,27 +1,111 @@
 import React from 'react';
 import Container from '../components/Container';
-import Row from '../components/Row';
-import Col from '../components/Col';
+import '../components/Card/style.css';
 
 function Discover() {
   return (
     <Container style={{ marginTop: 30 }}>
-    <Row>
-      <Col size="md-9">
-        <h1>Project Gallery</h1>
-      </Col>
-    </Row>
-    <Row>
-      <Col size="md-9">
-        <p>
-        I am a multi-disciplinary designer with a background in media, communication and culture. I enjoy bringing research to life visually and am experienced in crafting and enhancing content for various audiences and platforms; turning complex information into engaging and accessible stories and explaining technical issues to non-technical people. 
-</p><p>
-In my journey so far, I’ve had the pleasure of working with some amazing people: academics and policymakers in the UK; grassroots-based feminists and community radio broadcasters in the global south; teenagers learning how to write for the web; deaf students needing training in Photoshop; musicians, DJs, and artists all eager to share their valuable insights. 
-</p><p>
-I like learning new things and exploring new places. I am happiest when working in that sweet spot where technology, art and storytelling come together.            </p>
-        
-      </Col>
-    </Row>
+
+    <div className="container-fluid content">
+      {/* <h2 className="sectionHeading">Here's my work</h2>  */}
+    <div className="row"id="work">
+        {/* <!-- Card 1 --> */}
+        <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+          <div className="card">
+            <a href="https://code0em.github.io/lingua-sonnet/">
+            <div className="card-img-top-01"></div>  
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">Lingua Sonnet</h5>
+              <p className="card-text">
+              LinguaSonnet is a web application which seamlessly combines word learning with poetry, allowing users to access word definitions and examples of the word in a poem.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End card 1 --> */}
+
+        {/* <!-- Card 2 --> */}
+        <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+          <div className="card">
+            <a href="https://codeswitchstudio.github.io/horiseon-code-refactor/">
+            <div className="card-img-top-02"></div></a>
+            <div className="card-body">
+              <h5 className="card-title">Horiseon</h5>
+              <p className="card-text">
+                My first bootcamp project done by refactoring code to make a website that is more accessible.
+              </p>
+              
+            </div>
+          </div>
+        </div>
+        {/* <!-- End card 2 -->
+
+        <!-- Card 3 --> */}
+        <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+          <div className="card">
+            <a href="https://codeswitchstudio.com/pep/">
+            <div className="card-img-top-03"></div></a>
+            <div className="card-body">
+              <h5 className="card-title">Pep Manalang, artist</h5>
+              <p className="card-text">
+                A wordpress site for an abstract artist based in Fairbanks, Alaska 
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End card 3 -->
+
+        <!-- Card 4 --> */}
+        <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+          <div className="card">
+            <a href="https://codeswitchstudio.com/cajipe-endaya"> 
+            <div className="card-img-top-04"></div></a>
+            <div className="card-body">
+              <h5 className="card-title">Cajipe Endaya, landing page</h5>
+              <p className="card-text">
+                Landing page includes a CV, Press coverage page, and instagram embedding for the gallery.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End card 4 -->
+
+      <!-- Card 5 --> */}
+        <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+          <div className="card">
+            <a href="https://www.facebook.com/DevelopmentPraxis"> 
+            <div className="card-img-top-05"></div></a>
+            <div className="card-body">
+              <h5 className="card-title">DevPrax Brand Assets</h5>
+              <p className="card-text">
+              Development Praxis visual identity and assets featuring a vivid and clean logo to stand out in a sea of of social media posts.
+              </p>
+
+            </div>
+          </div>
+        </div>
+        {/* <!-- End card 5 -->  
+
+      <!-- Card 6 --> */}
+      <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+        <div className="card">
+          <a href="https://thecodingtrain.com/showcase/author/indicodeswitch">
+            <div className="card-img-top-06"></div></a>
+          <div className="card-body">
+            <h5 className="card-title">Creative Coding</h5>
+            <p className="card-text">
+            Experimental projects made from coding along with Dan Schiffman of The Coding Train's .
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <!-- End card 6 --> 
+
+
+    <!-- End cards --> */}
+      </div>
+    </div>
   </Container>
   );
 }
