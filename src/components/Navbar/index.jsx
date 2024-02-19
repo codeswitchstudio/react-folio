066f1fba4ad3e9032a83cfed16d0106a24caa38b
codeswitchstudio <toolbarista@gmail.com>
2024-02-19 15:24:00 +0000

    removed extraneous slash in CSS that creates netlify fails

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -44,8 +44,6 @@ function Navbar() {
                 About
               </NavLink>
             </li>
-
-
             <li className="nav-item">
               <NavLink
                 to="/contact"
